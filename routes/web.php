466bf276550e3363c32e989162c01df34ae8feb8
IlhamArifinnn <arifinn2208@gmail.com>
2024-07-11 20:02:30 +0700

    add landing page

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,28 @@
 <?php
 
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::get('/', [LandingPageController::class, 'index'])->name('index');
+Route::get('/about', [LandingPageController::class, 'about'])->name('about');
+Route::get('/contact', [LandingPageController::class, 'contact'])->name('contact');
+Route::get('/car', [LandingPageController::class, 'car'])->name('car');
+
+
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('welcome');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/products', [ProductController::class, 'index']);
 });
 
 require __DIR__ . '/auth.php';
