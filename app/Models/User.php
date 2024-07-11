faeb87affbcd95487bc9f9f5eb322fbc0f302401
IlhamArifinnn <arifinn2208@gmail.com>
2024-07-11 20:37:37 +0700

    solve login/register

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = ['fullname', 'username', 'password', 'email'];
+    protected $fillable = ['fullname', 'username', 'email', 'password'];
 
     public function loans()
     {
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('username');
+            $table->string('fullname')->default('fullname');
+            $table->string('username')->default('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
